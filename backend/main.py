1dd7d34d4488610556e49c3e983dfed7dfec1358
Leah Meromy <lmeromy@gmail.com>
2023-02-23 14:55:40 -0700

    Implement endpoints and basic UI requirements

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,35 @@
 from flask import Flask
-
+import geopandas as gpd
+import fiona
 import sqlite3
 
 
 app = Flask(__name__)
 
-conn = sqlite3.connect("tracts.gpkg")
 
 @app.route("/tracts")
 def list_tracts():
+
+    # gpkg =  './test.geojson'
+    file = gpd.read_file('geojson_tracts.geojson')
+    # with fiona.open('tracts.gpkg') as layer:
+    #     for feature in layer:
+    #         print(feature['geometry'])
+    # filtered = file.loc[file['NAMELSAD']]
+
     return {
-        "objects": [],
+       "tracts": file.to_json(),
+        
     }
 
 
 @app.route("/tracts/<int:pk>")
 def get_tract(pk):
+
+    geodataframe = gpd.read_file('geojson_tracts.geojson')
+
+    selected_tract =  geodataframe.loc[geodataframe['fid'] == pk, ["NAMELSAD", "GEOID", "geometry"]]
+    print(selected_tract.to_json())
     return {
-        "pk": pk,
+        "selected_tract":  selected_tract.to_json()
     }
